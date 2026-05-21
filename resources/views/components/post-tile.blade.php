a6cf5f527ede208c4071dd118cd27ab42bd925be
jpdoshi <jpdoshi2811@gmail.com>
2026-05-21 16:37:26 +0530

    component/post-tile: ui improvement

--- resources/views/components/post-tile.blade.php
+++ resources/views/components/post-tile.blade.php
@@ -1,23 +1,25 @@
 <a href="{{ '/posts/' . $post->id }}"
     class="border-b border-neutral-200 py-6 px-2 hover:translate-x-1 group duration-300 flex items-center justify-between gap-3">
     <div class="flex gap-5 items-center">
-        <img src="https://cdn3d.iconscout.com/3d/free/thumb/free-laravel-framework-logo-3d-icon-png-download-5453023.png"
-            class="size-12" />
+        <img src="{{ $post->poster_url }}"
+            class="h-30 aspect-video rounded-lg object-cover object-center border border-neutral-200" />
         <div>
             <h1
-                class="text-xl font-bold mb-1 leading-snug underline-offset-3 tracking-tight decoration-rose-200 group-hover:underline">
+                class="text-xl font-bold underline-offset-3 tracking-tight decoration-rose-200 group-hover:underline line-clamp-1 text-ellipsis">
                 {{ $post->title }}
             </h1>
-            <span>{{ $post->author }}</span>
+            <span class="text-rose-500 text-[15px] font-medium">{{ $post->author }}</span>
 
-            <p class="text-sm text-neutral-600 font-medium leading-normal tracking-normal">
+            <p class="mt-1 text-sm text-neutral-600 leading-normal tracking-normal line-clamp-2 text-ellipsis">
                 {{ $post->body }}
             </p>
         </div>
     </div>
 
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-        class="size-5 group-hover:text-rose-500">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-    </svg>
+    <div class="size-5">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+            class="size-5 group-hover:text-rose-500">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        </svg>
+    </div>
 </a>
